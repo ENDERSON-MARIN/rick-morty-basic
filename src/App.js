a7d2a72a959c3db16bf936d60453bdd52be9a777
Enderson Marín <46828206+ENDERSON-MARIN@users.noreply.github.com>
2022-04-18 15:26:13 -0500

    ADD IMG BG

--- src/App.js
+++ src/App.js
@@ -1,22 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import imageRickMorty from "./img/rick-morty.png";
+
+import "./App.css";
+import { useState } from "react";
+import Characters from "./components/Characters";
 
 function App() {
+  const [characters, setCharacters] = useState(null); //estado
+
+  const reqApi = async () => {
+    const api = await fetch("https://rickandmortyapi.com/api/character");
+
+    const characterApi = await api.json();
+    //console.log(characterApi);//respuesta completa de la API
+    setCharacters(characterApi.results);
+  };
+
+  //console.log(characters); //estado comienza inicializado en null
+  //console.log(characters); //estado al ser seteado con setCharacters ya contiene la informalcion de los personajes
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1 className="title">Rick & Morty</h1>
+        {characters ? ( //si el estado tiene información mostrar el componente Characters
+          <Characters characters={characters} setCharacters={setCharacters} />
+        ) : (
+          //sino mostrar la imagen y el botón para hacer la búsqueda
+          <>
+            <img className="img-home" src={imageRickMorty} alt="Rick & Morty" />
+            <button onClick={reqApi} className="btn-search">
+              Mostrar Personajes
+            </button>
+          </>
+        )}
       </header>
     </div>
   );
